82e9c83e05f8787e81c7aade74b9f4fc2197dc42
ericchen <dekknm0971@gmail.com>
2021-03-27 17:12:12 +0800

    add header

---
+++ src/views/header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-wrapper">
+    <Nav />
+    <Search />
+  </div>
+</template>
+
+<script>
+import Nav from '@/views/header/Nav'
+import Search from '@/views/header/Search'
+export default {
+  name: 'header',
+  components: {
+    Nav,
+    Search
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper{
+  background-color: $main-color;
+}
+</style>
